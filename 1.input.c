4a026de7a0db413c0cd9e4d651295d77f66399ac
Kefayet Ullah <150620881+KefayetUllah@users.noreply.github.com>
2025-07-07 21:27:39 +0600

    Add files via upload

---
+++ 1.input.c
@@ -0,0 +1,10 @@
+// This is a single-line comment
+#include <stdio.h> /* standard I/O */
+/*
+Multi-line comment
+should be removed
+*/
+int main() {
+printf("Hello, World!"); // print greeting
+return 0;
+}
