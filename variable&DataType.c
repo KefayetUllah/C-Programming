5a5f8148acddcf2b2b8d0ccfdb89ff8f20ee04e7
Kefayet Ullah <150620881+KefayetUllah@users.noreply.github.com>
2025-07-10 01:00:19 +0600

    Add files via upload

---
+++ variable&DataType.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int age = 25;
+float height = 5.9;
+char grade = 'A';
+double pi = 3.1415926535;
+printf("Age: %d, Height: %.1f, Grade: %c, PI: %.10lf\n", age, height,
+grade, pi);
+return 0;
+}
