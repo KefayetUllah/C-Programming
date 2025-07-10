3540b2cb67b6d69ec8b6d9af5c76430c1d59cbc7
Kefayet Ullah <150620881+KefayetUllah@users.noreply.github.com>
2025-07-11 01:39:31 +0600

    Add files via upload

---
+++ Pointers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num = 10;
+    int *ptr = &num;
+
+    printf("Value: %d\n", num);
+    printf("Address: %p\n", ptr);
+    printf("Value via Pointer: %d\n", *ptr);
+
+    return 0;
+}
